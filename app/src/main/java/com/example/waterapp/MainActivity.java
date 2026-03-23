7cfb08cdfb8624e1ced366f1a3cd701903ec0396
ZivL1 <another122122@gmail.com>
2026-03-23 20:17:11 +0200

    start screen where you enter name and gender

--- app/src/main/java/com/example/waterapp/MainActivity.java
+++ app/src/main/java/com/example/waterapp/MainActivity.java
@@ -1,6 +1,14 @@
 package com.example.waterapp;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +17,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
+    RadioGroup chooseGender;
+    RadioButton gender;
+    EditText name;
+    SharedPreferences sp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +33,48 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        initViews();
+        String checkName = sp.getString("name",null);
+        if(checkName!=null)
+        {
+            Intent intent = new Intent(MainActivity.this, mainApp.class);
+            startActivity(intent);
+            finish();
+        }
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int radioId = chooseGender.getCheckedRadioButtonId();
+                String sName = name.getText().toString();
+                gender = findViewById(radioId);
+                if(radioId == -1 || sName.trim().isEmpty())
+                {
+                    Toast.makeText(MainActivity.this, "השם או המגדר לא מולאו אנא מלא את הכל", Toast.LENGTH_LONG).show();
+                }else{
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putString("name",sName);
+                String sGender = "";
+                if(gender.getId() == R.id.rMale)
+                {
+                    sGender = "male";
+                }else if(gender.getId()==R.id.rFemale)
+                {
+                    sGender = "female";
+                }else
+                {
+                    sGender = "other";
+                }
+                editor.putString("gender",sGender);
+                editor.commit();
+                }
+            }
+        });
+    }
+
+    private void initViews() {
+        btn = findViewById(R.id.bFinishStart);
+        name = findViewById(R.id.etName);
+        chooseGender = findViewById(R.id.rChooseGender);
+        sp =getSharedPreferences("details",0);
     }
 }
